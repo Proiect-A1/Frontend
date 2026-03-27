c3f2a98dca07cb1f308743566a58c81631781024
lucal505 <lucaleonte@gmail.com>
2026-03-27 12:09:46 +0200

    clened up tracked files and updated gitignore:

--- fiicoder-frontend/src/types/problem.ts
+++
@@ -1,9 +0,0 @@
-export type Difficulty = "Ușor" | "Mediu" | "Greu";
-
-export interface Problem {
-  id: number;
-  title: string;
-  shortDescription: string;
-  statement: string;
-  difficulty: Difficulty;
-}
