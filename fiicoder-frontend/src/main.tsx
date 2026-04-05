71e45c839e51bcc568a2608fa759561c559020fb
lucal505 <lucaleonte@gmail.com>
2026-04-05 23:35:52 +0300

    import changed (cause being Refactoring Language.tsx)

--- fiicoder-frontend/src/main.tsx
+++ fiicoder-frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { LanguageProvider } from './language/LanguageUsed' 
+import { LanguageProvider } from './language/Language.tsx' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
