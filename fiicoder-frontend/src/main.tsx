bde5bc4915265dde80515c19d0c8537ccd132e78
lucal505 <lucaleonte@gmail.com>
2026-05-18 02:42:56 +0300

    moved notifications to right-bottom corner

--- fiicoder-frontend/src/main.tsx
+++ fiicoder-frontend/src/main.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-import App from './App.tsx'
-import { LanguageProvider } from './language/Language.tsx'
-import { AuthProvider } from './contexts/AuthContext'
-import { ThemeProvider } from './contexts/ThemeContext'
-import { queryClient } from './lib/queryClient'
-import { Toaster } from 'sonner'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
+import App from './App.tsx';
+import { LanguageProvider } from './language/Language.tsx';
+import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { queryClient } from './lib/queryClient';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <LanguageProvider>
-          <AuthProvider>
-            <App />
-            <Toaster position="top-right" richColors closeButton />
-          </AuthProvider>
-        </LanguageProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider>
+                <LanguageProvider>
+                    <AuthProvider>
+                        <App />
+                        <Toaster position="bottom-right" richColors closeButton />
+                    </AuthProvider>
+                </LanguageProvider>
+            </ThemeProvider>
+        </QueryClientProvider>
+    </StrictMode>,
+);
